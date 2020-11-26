e96861c8fb6eb43b38dd5c6ad235c6b232976fd2
Mariga123 <johnmariga8@gmail.com>
2020-11-26 10:50:22 +0300

    add review submit form,ability to see reviews posted and votes for each review

--- app/models/review.py
+++ app/models/review.py
@@ -1,6 +1,17 @@
 class Review:
 
     all_reviews = []
+    
+    @classmethod
+    def get_reviews(cls,id):
+
+        response = []
+
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+
+        return response
 
     def __init__(self,movie_id,title,imageurl,review):
         self.movie_id = movie_id
@@ -16,3 +27,5 @@ class Review:
     @classmethod
     def clear_reviews(cls):
         Review.all_reviews.clear()
+
+    
